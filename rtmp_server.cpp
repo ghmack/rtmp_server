e9a29caac0fdca2153c63c1919de3d9e18750c74
ghmack <1952476711@qq.com>
2015-04-06 21:01:55 +0800

    create program

---
+++ rtmp_server.cpp
@@ -0,0 +1,14 @@
+// rtmp_server.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "kernel/srs_kernel_log.hpp"
+
+ISrsLog* _srs_log = new ISrsLog();
+ISrsThreadContext* _srs_context = new ISrsThreadContext();
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
