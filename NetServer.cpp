a2df9cacad3709fbcc248b59585e2663c75519f6
ghmack <1952476711@qq.com>
2015-04-07 16:50:13 +0800

    NetServer file

---
+++ NetServer.cpp
@@ -0,0 +1,88 @@
+#include "NetServer.h"
+#include <stdio.h>
+
+RtmpNetServer::RtmpNetServer(string ip,int port):_ios(),_acceptor(_ios),_rtmpConPtr(new RtmpConnection(_ios))
+{
+	_ip = ip;
+	_port = port;
+}
+RtmpNetServer::~RtmpNetServer()
+{
+
+}
+
+
+void RtmpNetServer::start()
+{
+	boost::asio::ip::tcp::resolver reslver(_ios);
+	char szPort[20];
+	sprintf(szPort,"%d",_port);
+	boost::asio::ip::tcp::resolver::query query(_ip,szPort);
+	boost::asio::ip::tcp::endpoint endpoint = *reslver.resolve(query);
+	_acceptor.open(endpoint.protocol());
+	_acceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
+
+	_acceptor.bind(endpoint);
+	_acceptor.listen();
+	_acceptor.async_accept(_rtmpConPtr->socketRef(),boost::bind(&RtmpNetServer::handle_accept,
+		this,boost::asio::placeholders::error));
+
+}
+
+void RtmpNetServer::handle_accept(const boost::system::error_code& err)
+{
+	if (!err)
+	{
+		_rtmpConPtr->start();
+		_rtmpConPtr.reset(new RtmpConnection(_ios));
+		_acceptor.async_accept(_rtmpConPtr->socketRef(),boost::bind(&RtmpNetServer::handle_accept,
+			this,boost::asio::placeholders::error));
+	}
+	else
+	{
+		info_trace("accept socket error\r\n");
+	}
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+ConnectionMgr::ConnectionMgr()
+{
+
+}
+ConnectionMgr::~ConnectionMgr()
+{
+
+}
+
+void ConnectionMgr::add(RtmpConnection_ptr conPtr)
+{
+	_conSet.insert(conPtr);
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////
+RtmpConnection::RtmpConnection(boost::asio::io_service& ios):_socket(ios)
+{
+
+}
+RtmpConnection::~RtmpConnection()
+{
+
+}
+
+boost::asio::ip::tcp::socket& RtmpConnection::socketRef()
+{
+	return _socket;
+}
+
+void RtmpConnection::close()
+{
+	_socket.close();
+}
+
+void RtmpConnection::start()
+{
+
+}
---
+++ NetServer.h
@@ -0,0 +1,68 @@
+#pragma once
+
+
+#include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/bind.hpp>
+#include <string>
+#include <set>
+using namespace std;
+
+#define info_trace printf
+
+
+class RtmpConnection;
+class RtmpNetServer;
+
+
+
+
+
+class RtmpConnection : public enable_shared_from_this<RtmpConnection>
+{
+public:
+	RtmpConnection(boost::asio::io_service& ios);
+	~RtmpConnection();
+
+	boost::asio::ip::tcp::socket& socketRef();
+	void close();
+	void start();
+private:
+	boost::asio::ip::tcp::socket _socket;
+
+
+
+};
+
+typedef shared_ptr<RtmpConnection> RtmpConnection_ptr; 
+
+
+class RtmpNetServer
+{
+public:
+	RtmpNetServer(string ip,int port);
+	~RtmpNetServer();
+
+	void start();
+	void handle_accept(const boost::system::error_code& err);
+
+private:
+	boost::asio::io_service _ios;
+	boost::asio::ip::tcp::acceptor _acceptor;
+	string _ip;
+	int _port;
+	RtmpConnection_ptr _rtmpConPtr;
+
+};
+
+class ConnectionMgr
+{
+public:
+	ConnectionMgr();
+	~ConnectionMgr();
+	void add(RtmpConnection_ptr conPtr);
+private:
+
+	set<RtmpConnection_ptr> _conSet;
+};
